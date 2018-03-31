1e232ccedc523a5a398036f1204b432c3aca9df9
kdrag0n <dragon@khronodragon.com>
2018-03-30 21:43:28 -0700

    Add a command

--- modules/essential/constant.go
+++ modules/essential/constant.go
@@ -17,3 +17,11 @@ func (C) Test(c *core.Context) {
 
 	c.Session.MessageReactionAdd(c.Event.ChannelID, c.Event.ID, "👍")
 }
+
+// Owner explains the role of bot owner to the user.
+func (C) Owner(c *core.Context) {
+	c.Info("Become bot owner.") // Lure them in
+
+	c.Session.ChannelMessageSend(c.Event.ChannelID, `My owner is **kdragon#1337**. It's something that affects entire bot, and is the person who actually owns the bot, not the owner of a server.
+**No**, you may not have bot owner because it allows full control. In your server, being server owner is sufficient.`)
+}
